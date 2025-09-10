94ac06f97809419d4ebb10df5fb74972f69e796b
Link-Spirit <terryvaneijk55@yahoo.com>
2025-09-10 09:19:34 -0500

    Update WordManipulator.cs

--- WordManipulator.cs
+++ WordManipulator.cs
@@ -8,37 +8,44 @@ namespace WordReverser {
     internal class WordManipulator {
         public static string ReverseWords(string input) {
             string word = "";
-            List<string> ReverseOrder = [];
-            List<string> words = [];
+            List<string> words = new List<string>();
 
-            for(int i = 0; i < input.Count(); i++) {
+            for(int i = 0; i < input.Length(); i++) { // use length instead of count
                 
-                if(char.IsLetter(input[i])) { 
+                if(char.IsWhiteSpace(input[i])) {  //change char.IsLetter
                     word = word + input[i];
                 }
                 else { 
+                    if(word.Length > 0)
+                    {
                     words.Add(word);
                     word = "";
                 }    
             }
-
-
-            List<string> reverseWordOrder = [];
+            }
+            if (word.Length>0) // checks if the last word exist 
+            {
+                words.Add(word);
+            }
+            List<string> reverseWordOrder = new List<string>();
 
             for(int i = words.Count(); i > 0; i--) {
-                if(!string.IsNullOrWhiteSpace(words[i-1])) {
+                if(!string.IsNullOrWhiteSpace(words[i-1])) 
+                {
                     reverseWordOrder.Add(words[i-1]);
                 }
             }
 
             string Results = "";
 
-            for(int i = 0; i < reverseWordOrder.Count(); i++) {
+            for(int i = 0; i < reverseWordOrder.Count; i++) 
+            {
                 Results = Results + reverseWordOrder[i] + " ";
             }
-
+            if (Results.Length > 0) //Trims Substrings
+            {
             Results = Results.Substring(0, Results.Length - 1);
-
+            }
             return Results;
         }
     }
